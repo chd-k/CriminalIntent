4bff08f0c9ea0a5dea733d802dca3637951be91c
chd-k <asgribovskaya@yandex.ru>
2022-07-04 01:02:24 +0300

    init commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.criminalintent
+
+import java.util.UUID
+import java.util.Date
+import java.util.*
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
