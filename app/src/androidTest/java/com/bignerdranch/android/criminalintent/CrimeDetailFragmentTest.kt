d89543ea31c9f542e749b73ce82e2d15ee0174c8
chd-k <asgribovskaya@yandex.ru>
2022-07-06 01:36:34 +0300

    add RecyclerView

---
+++ app/src/androidTest/java/com/bignerdranch/android/criminalintent/CrimeDetailFragmentTest.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalintent
+
+import org.junit.Assert.*
+import androidx.test.espresso.Espresso
+import org.junit.After
+import org.junit.Before
+
+// TODO: do tests 
+
+class CrimeDetailFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
